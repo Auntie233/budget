c933c66c0187fa54dfc17a693d2ad7c0bdf38a61
hahama01 <hahama01@gmail.com>
2020-08-19 15:01:09 +0800

    budget

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,81 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <el-row>
+      <el-input-number v-model="num" :min="1" label="预算"></el-input-number>
+      <el-button type="primary" icon="el-icon-edit" circle @click="addRow"></el-button>
+    </el-row>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="name" label="产品名称" width="120">
+        <template slot-scope="scope">
+          <el-input @blur="scope.row.nameShow = !scope.row.nameShow" v-if="scope.row.nameShow" v-model="scope.row.name"></el-input>
+          <span v-else @click="scope.row.nameShow = !scope.row.nameShow">{{scope.row.name}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="price" label="产品单价" width="120">
+        <template slot-scope="scope">
+          <el-input @blur="scope.row.priceShow = !scope.row.priceShow" v-if="scope.row.priceShow" v-model="scope.row.price"></el-input>
+          <span v-else @click="scope.row.priceShow = !scope.row.priceShow">{{scope.row.price}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="count" label="数量">
+        <template slot-scope="scope">
+          <el-slider v-model="scope.row.count" show-stops :max="num/scope.row.price" @input="changeCount(scope.row)" show-input></el-slider>
+        </template>
+      </el-table-column>
+      <el-table-column prop="amount" label="产品总额" width="120"></el-table-column>
+    </el-table>
+    <span>总花费{{cost}}</span>
   </div>
+
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data: function () {
+    return {
+      value: 10,
+      num: 0,
+      cost:0,
+      tableData:[
+        {
+          name:"default",
+          price:1,
+          count:0,
+          amount:0,
+          priceShow: false,
+          nameShow: false
+        }
+      ]
+    }
+  },
+  methods: {
+    changeCount: function (scope) {
+      this.max = this.num
+      scope.amount=(scope.price*scope.count);
+      this.cost=0
+      for (let index in this.tableData) {
+        this.cost = this.cost+this.tableData[index].amount
+      }
+      if (this.cost>this.num) {
+        scope.count = scope.count-1
+      }
+    },
+    addRow() {
+      let newRow = {
+        name:"new",
+        price:1,
+        count:0,
+        amount:0,
+        priceShow: false,
+        nameShow: false
+      }
+      this.tableData.push(newRow)
+    }
+  },
   props: {
     msg: String
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
